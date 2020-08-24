896f064e754322acf64004a9d3c4c2512906991e
a48446 <ya0933632805@gmail.com>
2020-08-24 17:05:55 +0800

    make it better

--- echobot/views.py
+++ echobot/views.py
@@ -1,13 +1,11 @@
 from django.conf import settings
 from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
-
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import requests
 from selenium import webdriver
-
 from selenium.webdriver.chrome.options import Options
 
 line_bot_api = LineBotApi('ZnFx4dn7xECPlisBxNk16yZS2bOphDdMNKCXm2VOMaHHtjKbEkfWTt6fxDVAZVuXl5Wr3CIHE4J3FYeqv/I/fPJ3UBCjYAAhlZK2hSgw3LN8JeN2BqHVZsW9CEPHRtQRCM7ZzI/zm3/owADNERLy4QdB04t89/1O/w1cDnyilFU=')
@@ -34,7 +32,9 @@ def callback(request: HttpRequest) -> HttpResponse:
     else:
         return HttpResponseBadRequest()
 
-def weatherstatus(self):
+
+@handler.add(MessageEvent, message=TextMessage)
+def message_text(event: MessageEvent):
     options = Options()
     #關閉瀏覽器跳出訊息
     prefs = {
@@ -55,15 +55,10 @@ def weatherstatus(self):
     Sunrise = driver.find_element_by_id('GT_Sunrise').text
     Sunset = driver.find_element_by_id('GT_Sunset').text
     driver.quit()
+
     content="\n"+"名間鄉天氣狀況"+"\n"+"\n"+"現在溫度 : "+Temp+"°C"+"\n"+"體感溫度 : "+bodyTemp+"°C"+"\n"+"相對溼度 : "+RelativeHumidity+"%"+"\n"+"降雨量 : "+Rain+"mm"+"\n"+"日出時間 : "+Sunrise+"\n"+"日落時間 : "+Sunset
 
-@handler.add(MessageEvent, message=TextMessage)
-def message_text(event: MessageEvent):
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(weatherstatus.content))
-
-# @handler.add(MessageEvent, message=TextMessage)
-# def message_text(event: MessageEvent):
-#     line_bot_api.reply_message(
-#         event.reply_token,
-#         TextSendMessage(text=event.message.text)
-#     )
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(content)
+    )
