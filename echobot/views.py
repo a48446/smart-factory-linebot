7cbb11bb365dee875de4d97cb173ebbcba693d3f
a48446 <ya0933632805@gmail.com>
2020-08-27 15:53:55 +0800

    make it better

--- echobot/views.py
+++ echobot/views.py
@@ -13,17 +13,25 @@ parser = WebhookParser('57141ec8f7ba725d4fa3fa97a5bd5169')
 
 
 @csrf_exempt
-def callback(request: HttpRequest) -> HttpResponse:
-    
-    if request.method == "POST":
+def callback(request):
 
+    if request.method == 'POST':
         signature = request.META['HTTP_X_LINE_SIGNATURE']
         body = request.body.decode('utf-8')
+
         try:
-            handler.handle(body, signature)
+            events = parser.parse(body, signature)
         except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
             return HttpResponseBadRequest()
 
+        for event in events:
+            if isinstance(event, MessageEvent):
+                line_bot_api.reply_message(
+                    event.reply_token,
+                   TextSendMessage(text=event.message.text)
+                )
         return HttpResponse()
     else:
         return HttpResponseBadRequest()
@@ -63,12 +71,3 @@ def crawler():
     else:
         message = {"no"}
         return message
-
-
-@handler.add(MessageEvent, message=TextMessage)
-def message_text(event):
-    a = crawler()
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text='helloworld')
-    )
