21271ddb6faef39492a2eb320e293f20ac73f465
a48446 <ya0933632805@gmail.com>
2020-08-27 16:29:10 +0800

    make it better

--- echobot/views.py
+++ echobot/views.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest
+from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest , HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
@@ -8,22 +8,32 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
+
 line_bot_api = LineBotApi('q7TWa/81a0nmW9GnqF6+u8qaFoMbi6q3Dq5VK2QM7FV8UIx3nQk5+luk5GpASk/bm5qtAmimAyA2/Ifdg6a0hH3dwMdfdAoRiGE8TF/IiRXriLsK7j9FDHlQUC34zr7EXiktLqyT5btGhtCTJXbTZQdB04t89/1O/w1cDnyilFU=')
-parser = WebhookParser('57141ec8f7ba725d4fa3fa97a5bd5169')
+parser = WebhookHandler("57141ec8f7ba725d4fa3fa97a5bd5169")
 
 
 @csrf_exempt
 def callback(request):
-    
-    if request.method == "POST":
 
+    if request.method == 'POST':
         signature = request.META['HTTP_X_LINE_SIGNATURE']
         body = request.body.decode('utf-8')
+
         try:
-            handler.handle(body, signature)
+            events = parser.parse(body, signature)
         except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
             return HttpResponseBadRequest()
 
+        for event in events:
+            if isinstance(event, MessageEvent):
+                line_bot_api.reply_message(
+                    event.reply_token,
+                   TextSendMessage(text=event.message.text)
+                )
         return HttpResponse()
     else:
         return HttpResponseBadRequest()
@@ -65,10 +75,10 @@ def crawler():
         return message
 
 
-@handler.add(MessageEvent, message=TextMessage)
-def message_text(event):
-    a = crawler()
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text="hello")
-    )
+# @handler.add(MessageEvent, message=TextMessage)
+# def message_text(event):
+#     a = crawler()
+#     line_bot_api.reply_message(
+#         event.reply_token,
+#         TextSendMessage(text="hello")
+#     )
