8ac3c9c0c5a7f9005e2bc7537d1736de06aa313e
a48446 <ya0933632805@gmail.com>
2020-08-27 18:31:42 +0800

    make it better

--- echobot/views.py
+++ echobot/views.py
@@ -1,33 +1,31 @@
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
-from linebot import LineBotApi, WebhookParser
+from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import MessageEvent, TextSendMessage
-from module import func
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 line_bot_api = LineBotApi('q7TWa/81a0nmW9GnqF6+u8qaFoMbi6q3Dq5VK2QM7FV8UIx3nQk5+luk5GpASk/bm5qtAmimAyA2/Ifdg6a0hH3dwMdfdAoRiGE8TF/IiRXriLsK7j9FDHlQUC34zr7EXiktLqyT5btGhtCTJXbTZQdB04t89/1O/w1cDnyilFU=')
-parser = WebhookParser("57141ec8f7ba725d4fa3fa97a5bd5169")
+handler = WebhookHandler("57141ec8f7ba725d4fa3fa97a5bd5169")
 
-
-#傳什麼回什麼範例
 @csrf_exempt
 def callback(request):
     if request.method == 'POST':
         signature = request.META['HTTP_X_LINE_SIGNATURE']
         body = request.body.decode('utf-8')
-
         try:
-            events = parser.parse(body, signature)
+            handler.handle(body, signature)
         except InvalidSignatureError:
-            return HttpResponseForbidden()
-        except LineBotApiError:
-            return HttpResponseBadRequest()
+            messages = (
+                "Invalid signature. Please check your channel access token/channel secret."
+            )
+            logger.error(messages)
+            return HttpResponseBadRequest(messages)
+        return HttpResponse("OK")
 
-        for event in events:
-            if isinstance(event, MessageEvent):
-                line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
-
-        return HttpResponse()
-    else:
-        return HttpResponseBadRequest()
+@handler.add(event=MessageEvent, message=TextMessage)
+def handl_message(event: MessageEvent):
+    line_bot_api.reply_message(
+        reply_token=event.reply_token,
+        messages=TextSendMessage(text=event.message.text),
+    )
