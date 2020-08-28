8ce8a42c7d1173f3aa87522e560b7c68316b9a6f
a48446 <ya0933632805@gmail.com>
2020-08-28 11:52:18 +0800

    make it better

--- text.py
+++ text.py
@@ -37,14 +37,7 @@ def crawler():
         Sunrise = driver.find_element_by_id('GT_Sunrise').text #日出時間
         Sunset = driver.find_element_by_id('GT_Sunset').text
         driver.quit()
-        content= {
-            "現在溫度":Temp,
-            "體感溫度":bodyTemp,
-            "相對溼度":RelativeHumidity,
-            "降雨量":Rain,
-            "日出時間":Sunrise,
-            "日落時間":Sunset
-                }
+        content="\n"+"名間鄉天氣概況"+"\n"+"\n"+"現在溫度 : "+Temp+"°C"+"\n"+"體感溫度 : "+bodyTemp+"°C"+"\n"+"相對溼度 : "+RelativeHumidity+"%"+"\n"+"降雨量 : "+Rain+"mm"+"\n"+"日出時間 : "+Sunrise+"\n"+"日落時間 : "+Sunset
         # print(content)
         return content
     else:
