22f24d7045b101cccdc2888566ee7d318b9dcb73
a48446 <ya0933632805@gmail.com>
2020-08-28 13:42:03 +0800

    make it better

--- echobot/views.py
+++ echobot/views.py
@@ -12,28 +12,6 @@ from selenium.webdriver.chrome.options import Options
 line_bot_api = LineBotApi('q7TWa/81a0nmW9GnqF6+u8qaFoMbi6q3Dq5VK2QM7FV8UIx3nQk5+luk5GpASk/bm5qtAmimAyA2/Ifdg6a0hH3dwMdfdAoRiGE8TF/IiRXriLsK7j9FDHlQUC34zr7EXiktLqyT5btGhtCTJXbTZQdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler("57141ec8f7ba725d4fa3fa97a5bd5169")
 
-prefs = {  
-            'profile.default_content_setting_values' :  {  
-                'notifications' : 2  
-                     }  
-                }  
-chrome_options.add_experimental_option('prefs',prefs)
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")      #不開啟實體瀏覽器背景執行
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-driver.get("https://www.cwb.gov.tw/V8/C/W/Town/Town.html?TID=1000806") #南投名間鄉
-Temp = driver.find_element_by_id('GT_C_T').text #現在溫度
-bodyTemp = driver.find_element_by_id('GT_C_AT').text #體感溫度
-RelativeHumidity = driver.find_element_by_id('GT_RH').text #相對溼度
-Rain = driver.find_element_by_id('GT_Rain').text #降雨量
-Sunrise = driver.find_element_by_id('GT_Sunrise').text #日出時間
-Sunset = driver.find_element_by_id('GT_Sunset').text
-driver.quit()
-content="\n"+"名間鄉天氣概況"+"\n"+"\n"+"現在溫度 : "+Temp+"°C"+"\n"+"體感溫度 : "+bodyTemp+"°C"+"\n"+"相對溼度 : "+RelativeHumidity+"%"+"\n"+"降雨量 : "+Rain+"mm"+"\n"+"日出時間 : "+Sunrise+"\n"+"日落時間 : "+Sunset
-
 @csrf_exempt
 def callback(request):
     if request.method == 'POST':
@@ -50,9 +28,9 @@ def callback(request):
         return HttpResponse("OK")
 
 @handler.add(event=MessageEvent, message=TextMessage)
-def handl_message(event: MessageEvent):
+def handl_message(event: MessageEvent,content):
     
-    reply = TextSendMessage(text=content)
+    reply = TextSendMessage(text='你好')
     line_bot_api.reply_message(
         reply_token=event.reply_token,
         messages=reply
