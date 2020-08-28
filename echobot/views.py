25ad15e87fd62ee92e83bb88fe0d3c12fc33acd0
a48446 <ya0933632805@gmail.com>
2020-08-28 13:44:19 +0800

    make it better

--- echobot/views.py
+++ echobot/views.py
@@ -28,8 +28,28 @@ def callback(request):
         return HttpResponse("OK")
 
 @handler.add(event=MessageEvent, message=TextMessage)
-def handl_message(event: MessageEvent,content):
-    
+def handl_message(event: MessageEvent):
+    prefs = {  
+            'profile.default_content_setting_values' :  {  
+                'notifications' : 2  
+                     }  
+                }  
+    chrome_options.add_experimental_option('prefs',prefs)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")      #不開啟實體瀏覽器背景執行
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    driver.get("https://www.cwb.gov.tw/V8/C/W/Town/Town.html?TID=1000806") #南投名間鄉
+    Temp = driver.find_element_by_id('GT_C_T').text #現在溫度
+    bodyTemp = driver.find_element_by_id('GT_C_AT').text #體感溫度
+    RelativeHumidity = driver.find_element_by_id('GT_RH').text #相對溼度
+    Rain = driver.find_element_by_id('GT_Rain').text #降雨量
+    Sunrise = driver.find_element_by_id('GT_Sunrise').text #日出時間
+    Sunset = driver.find_element_by_id('GT_Sunset').text
+    driver.quit()
+    content="\n"+"名間鄉天氣概況"+"\n"+"\n"+"現在溫度 : "+Temp+"°C"+"\n"+"體感溫度 : "+bodyTemp+"°C"+"\n"+"相對溼度 : "+RelativeHumidity+"%"+"\n"+"降雨量 : "+Rain+"mm"+"\n"+"日出時間 : "+Sunrise+"\n"+"日落時間 : "+Sunset
     reply = TextSendMessage(text='你好')
     line_bot_api.reply_message(
         reply_token=event.reply_token,
