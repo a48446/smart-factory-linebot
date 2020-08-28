9335819e53c0f4b08d09c233133f5857cd427af2
a48446 <ya0933632805@gmail.com>
2020-08-28 15:23:14 +0800

    make it better

--- echobot/views.py
+++ echobot/views.py
@@ -30,7 +30,7 @@ def callback(request):
 
 @handler.add(event=MessageEvent, message=TextMessage)
 def handl_message(event):
-    outInfo = json.dumps({'a': '123', 'b': '123'})
+    outInfo = "123\n456"
     # outInfo = json.dumps(crawler())
     message = TextSendMessage(text=outInfo)
     line_bot_api.reply_message(
