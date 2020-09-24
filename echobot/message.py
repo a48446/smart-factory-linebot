fc39b6b10ac068519e82870f9e471f524f13c698
a48446 <ya0933632805@gmail.com>
2020-09-24 15:27:11 +0800

    0123

--- echobot/message.py
+++ echobot/message.py
@@ -281,83 +281,76 @@ class textmodel():
         flex_message = FlexSendMessage(
             alt_text='hello',
             contents={
-                "type": "bubble",
-                "hero": {
-                    "type": "image",
-                    "url": "https://i.imgur.com/O8lp0mk.png",
-                    "size": "full",
-                    "aspectRatio": "20:13",
-                    "aspectMode": "fit",
-                    "action": {
-                    "type": "uri",
-                    "uri": "http://linecorp.com/"
-                    },
-                    "backgroundColor": "#FFEE99"
+            "type": "bubble",
+            "hero": {
+                "type": "image",
+                "url": "https://i.imgur.com/O8lp0mk.png",
+                "size": "full",
+                "backgroundColor": "#FFEE99"
+            },
+            "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                {
+                    "type": "text",
+                    "text": "排風風扇",
+                    "weight": "bold",
+                    "size": "xl"
                 },
-                "body": {
+                {
                     "type": "box",
-                    "layout": "vertical",
+                    "layout": "baseline",
+                    "margin": "md",
                     "contents": [
                     {
                         "type": "text",
-                        "text": "排風風扇",
-                        "weight": "bold",
-                        "size": "xl"
-                    },
-                    {
-                        "type": "box",
-                        "layout": "baseline",
-                        "margin": "md",
-                        "contents": [
-                        {
-                            "type": "text",
-                            "text": "狀態：",
-                            "size": "xs",
-                            "color": "#999999",
-                            "margin": "xs",
-                            "flex": 0
-                        }
-                        ]
-                    },
-                    {
-                        "type": "box",
-                        "layout": "vertical",
-                        "margin": "lg",
-                        "spacing": "sm",
-                        "contents": []
+                        "text": "狀態：",
+                        "size": "xs",
+                        "color": "#999999",
+                        "margin": "xs",
+                        "flex": 0
                     }
                     ]
                 },
-                "footer": {
+                {
                     "type": "box",
                     "layout": "vertical",
+                    "margin": "lg",
                     "spacing": "sm",
-                    "contents": [
-                    {
-                        "type": "button",
-                        "style": "link",
-                        "height": "sm",
-                        "action": {
-                        "type": "postback",
-                        "label": "開啟",
-                        "data": "開啟",
-                        "displayText": "開啟"
-                        }
-                    },
-                    {
-                        "type": "button",
-                        "style": "link",
-                        "height": "sm",
-                        "action": {
-                        "type": "postback",
-                        "label": "關閉",
-                        "data": "關閉",
-                        "displayText": "關閉"
-                        }
-                    }
-                    ],
-                    "flex": 0
+                    "contents": []
                 }
+                ]
+            },
+            "footer": {
+                "type": "box",
+                "layout": "vertical",
+                "spacing": "sm",
+                "contents": [
+                {
+                    "type": "button",
+                    "style": "link",
+                    "height": "sm",
+                    "action": {
+                    "type": "postback",
+                    "label": "開啟",
+                    "data": "開啟",
+                    "displayText": "開啟"
+                    }
+                },
+                {
+                    "type": "button",
+                    "style": "link",
+                    "height": "sm",
+                    "action": {
+                    "type": "postback",
+                    "label": "關閉",
+                    "data": "關閉",
+                    "displayText": "關閉"
+                    }
+                }
+                ],
+                "flex": 0
             }
-        )
+            })
         return flex_message
