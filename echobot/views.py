f8f2c9fd403b06f6748f6f290d74809a833802f4
a48446 <ya0933632805@gmail.com>
2020-09-28 14:00:03 +0800

    0123

--- echobot/views.py
+++ echobot/views.py
@@ -58,18 +58,74 @@ def callback(request):
                     line_bot_api.reply_message(
                         event.reply_token,
                         message)
+                    VCPUnewvalue = []
                     VCPUnewvalue = event.message.text
-                    line_bot_api.reply_message(  # 回復「設定機房資訊」VCPU更改訊息
-                        event.reply_token,
-                        returna
-                    )
-                    if event.message.text == "yes":
-                        myquery = { "_id": ObjectId(data_objectid)}
-                        newvalues = { "$set": { 
-                                        "disk":VCPUnewvalue
+                    flex_message = FlexSendMessage(
+                        alt_text='hello',
+                        contents={
+                        "type": "bubble",
+                        "header": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                            {
+                                "type": "text",
+                                "text": "VCPU數量(顆):" + VCPUnewvalue
+                            }
+                            ],
+                            "backgroundColor": "#F0F0F0"
+                        },
+                        "body": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                            {
+                                "type": "box",
+                                "layout": "vertical",
+                                "contents": []
+                            },
+                            {
+                                "type": "box",
+                                "layout": "vertical",
+                                "contents": [
+                                {
+                                    "type": "button",
+                                    "action": {
+                                    "type": "message",
+                                    "label": "yes",
+                                    "text": "yes"+'\n'+"接著設定機房資訊"+'\n'+'機房儲存空間(TB)'
+                                    },
+                                    "position": "absolute",
+                                    "offsetStart": "20px"
+                                },
+                                {
+                                    "type": "button",
+                                    "action": {
+                                    "type": "message",
+                                    "label": "no",
+                                    "text": "no"
+                                    },
+                                    "position": "relative",
+                                    "offsetStart": "70px"
+                                }
+                                ]
+                            }
+                            ]
+                        }
+                        }
+                        )
+                    if 1:
+                        line_bot_api.reply_message(  # 回復「設定機房資訊」VCPU更改訊息
+                            event.reply_token,
+                            flex_message
+                            )
+                        if event.message.text == "yes":
+                            myquery = { "_id": ObjectId(data_objectid)}
+                            newvalues = { "$set": { 
+                                            "disk":VCPUnewvalue
+                                                }
                                             }
-                                        }
-                        RoomInformationdata.update_one(myquery, newvalues)
+                            RoomInformationdata.update_one(myquery, newvalues)
                             # RAMnewvalue = input("請輸入RAM數量(GB): ")
                         # #     if RAMnewvalue != None:
                         #         line_bot_api.reply_message(  # 回復「設定機房資訊」VCPU更改訊息
@@ -150,119 +206,119 @@ def callback(request):
         return HttpResponseBadRequest()
 
 # 設定機房資訊回傳
-def returna(self):
-    flex_message = FlexSendMessage(
-        alt_text='hello',
-        contents={
-        "type": "bubble",
-        "header": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-            {
-                "type": "text",
-                "text": "VCPU數量(顆):"+ callback('VCPUnewvalue')
-            }
-            ],
-            "backgroundColor": "#F0F0F0"
-        },
-        "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-            {
-                "type": "box",
-                "layout": "vertical",
-                "contents": []
-            },
-            {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [
-                {
-                    "type": "button",
-                    "action": {
-                    "type": "message",
-                    "label": "yes",
-                    "text": "yes"+'\n'+"接著設定機房資訊"+'\n'+'機房儲存空間(TB)'
-                    },
-                    "position": "absolute",
-                    "offsetStart": "20px"
-                },
-                {
-                    "type": "button",
-                    "action": {
-                    "type": "message",
-                    "label": "no",
-                    "text": "no"
-                    },
-                    "position": "relative",
-                    "offsetStart": "70px"
-                }
-                ]
-            }
-            ]
-        }
-        }
-    )
-    return flex_message
+# def returna(self):
+#     flex_message = FlexSendMessage(
+#         alt_text='hello',
+#         contents={
+#         "type": "bubble",
+#         "header": {
+#             "type": "box",
+#             "layout": "vertical",
+#             "contents": [
+#             {
+#                 "type": "text",
+#                 "text": "VCPU數量(顆):" + 
+#             }
+#             ],
+#             "backgroundColor": "#F0F0F0"
+#         },
+#         "body": {
+#             "type": "box",
+#             "layout": "vertical",
+#             "contents": [
+#             {
+#                 "type": "box",
+#                 "layout": "vertical",
+#                 "contents": []
+#             },
+#             {
+#                 "type": "box",
+#                 "layout": "vertical",
+#                 "contents": [
+#                 {
+#                     "type": "button",
+#                     "action": {
+#                     "type": "message",
+#                     "label": "yes",
+#                     "text": "yes"+'\n'+"接著設定機房資訊"+'\n'+'機房儲存空間(TB)'
+#                     },
+#                     "position": "absolute",
+#                     "offsetStart": "20px"
+#                 },
+#                 {
+#                     "type": "button",
+#                     "action": {
+#                     "type": "message",
+#                     "label": "no",
+#                     "text": "no"
+#                     },
+#                     "position": "relative",
+#                     "offsetStart": "70px"
+#                 }
+#                 ]
+#             }
+#             ]
+#         }
+#         }
+#     )
+#     return flex_message
 
-def returnb(self):
-    flex_message = FlexSendMessage(
-        alt_text='hello',
-        contents={
-        "type": "bubble",
-        "header": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-            {
-                "type": "text",
-                "text": "RAM數量(GB):"+callback('RAMnewvalue')
-            }
-            ],
-            "backgroundColor": "#F0F0F0"
-        },
-        "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-            {
-                "type": "box",
-                "layout": "vertical",
-                "contents": []
-            },
-            {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [
-                {
-                    "type": "button",
-                    "action": {
-                    "type": "message",
-                    "label": "yes",
-                    "text": "yes"+'\n'+"接著設定機房資訊"+'\n'+'機房Switch數量(台)'
-                    },
-                    "position": "absolute",
-                    "offsetStart": "20px"
-                },
-                {
-                    "type": "button",
-                    "action": {
-                    "type": "message",
-                    "label": "no",
-                    "text": "no"
-                    },
-                    "position": "relative",
-                    "offsetStart": "70px"
-                }
-                ]
-            }
-            ]
-        }
-        }
-    )
-    return flex_message 
+# def returnb(self):
+#     flex_message = FlexSendMessage(
+#         alt_text='hello',
+#         contents={
+#         "type": "bubble",
+#         "header": {
+#             "type": "box",
+#             "layout": "vertical",
+#             "contents": [
+#             {
+#                 "type": "text",
+#                 "text": "RAM數量(GB):"
+#             }
+#             ],
+#             "backgroundColor": "#F0F0F0"
+#         },
+#         "body": {
+#             "type": "box",
+#             "layout": "vertical",
+#             "contents": [
+#             {
+#                 "type": "box",
+#                 "layout": "vertical",
+#                 "contents": []
+#             },
+#             {
+#                 "type": "box",
+#                 "layout": "vertical",
+#                 "contents": [
+#                 {
+#                     "type": "button",
+#                     "action": {
+#                     "type": "message",
+#                     "label": "yes",
+#                     "text": "yes"+'\n'+"接著設定機房資訊"+'\n'+'機房Switch數量(台)'
+#                     },
+#                     "position": "absolute",
+#                     "offsetStart": "20px"
+#                 },
+#                 {
+#                     "type": "button",
+#                     "action": {
+#                     "type": "message",
+#                     "label": "no",
+#                     "text": "no"
+#                     },
+#                     "position": "relative",
+#                     "offsetStart": "70px"
+#                 }
+#                 ]
+#             }
+#             ]
+#         }
+#         }
+#     )
+#     return flex_message 
 
 # @csrf_exempt
 # def callback(request):
